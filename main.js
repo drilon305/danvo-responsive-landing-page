28b66443de6e961e53ad3a64057084a7d3a9c08f
drilon305 <drilonsulejmani305@gmail.com>
2022-07-18 00:59:30 +0200

    responsive design

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var toggle_btn;
 var big_wrapper;
+var hamburger_menu;
 
 function declare() {
     toggle_btn = document.querySelector('.toggle-btn');
     big_wrapper = document.querySelector('.big-wrapper');
+    hamburger_menu = document.querySelector('.hamburger-menu')
 }
 
 const main = document.querySelector('main')
@@ -38,7 +40,10 @@ function toggleAnimation() {
 }
 
 function events() {
-    toggle_btn.addEventListener('click', toggleAnimation)
+    toggle_btn.addEventListener('click', toggleAnimation);
+    hamburger_menu.addEventListener('click', () => {
+        big_wrapper.classList.toggle('active')
+    })
 }
 
 events()
